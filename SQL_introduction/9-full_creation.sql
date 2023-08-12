fe032a13ad6bdd0aaa1aa22ba58284e3d903a467
amostungu <amostungu1@gmail.com>
2023-08-12 10:25:26 -0700

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,13 +1,13 @@
 -- Create the table second_table if it doesn't exist
+USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 
--- Insert records into the second_table (without SELECT or SHOW statements)
-INSERT INTO second_table (id, name, score) SELECT * FROM (SELECT 1, 'John', 10) AS tmp WHERE NOT EXISTS (SELECT id FROM second_table WHERE id = 1) LIMIT 1;
-INSERT INTO second_table (id, name, score) SELECT * FROM (SELECT 2, 'Alex', 3) AS tmp WHERE NOT EXISTS (SELECT id FROM second_table WHERE id = 2) LIMIT 1;
-INSERT INTO second_table (id, name, score) SELECT * FROM (SELECT 3, 'Bob', 14) AS tmp WHERE NOT EXISTS (SELECT id FROM second_table WHERE id = 3) LIMIT 1;
-INSERT INTO second_table (id, name, score) SELECT * FROM (SELECT 4, 'George', 8) AS tmp WHERE NOT EXISTS (SELECT id FROM second_table WHERE id = 4) LIMIT 1;
-
+-- Insert records into the second_table
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
