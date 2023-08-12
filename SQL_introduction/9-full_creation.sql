62e5df8667b6b2c0ec8eb9de96bb14a5a80ca533
amostungu <amostungu1@gmail.com>
2023-08-12 10:22:19 -0700

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,13 @@
--- a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table(id, name, score) VALUES
-(1, "John", 10),
-(2, "Alex" 3),
-(3, "Bob", 14),
-(4, "George", 8);
+-- Create the table second_table if it doesn't exist
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- Insert records into the second_table (without SELECT or SHOW statements)
+INSERT INTO second_table (id, name, score) SELECT * FROM (SELECT 1, 'John', 10) AS tmp WHERE NOT EXISTS (SELECT id FROM second_table WHERE id = 1) LIMIT 1;
+INSERT INTO second_table (id, name, score) SELECT * FROM (SELECT 2, 'Alex', 3) AS tmp WHERE NOT EXISTS (SELECT id FROM second_table WHERE id = 2) LIMIT 1;
+INSERT INTO second_table (id, name, score) SELECT * FROM (SELECT 3, 'Bob', 14) AS tmp WHERE NOT EXISTS (SELECT id FROM second_table WHERE id = 3) LIMIT 1;
+INSERT INTO second_table (id, name, score) SELECT * FROM (SELECT 4, 'George', 8) AS tmp WHERE NOT EXISTS (SELECT id FROM second_table WHERE id = 4) LIMIT 1;
+
