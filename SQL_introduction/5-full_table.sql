4ab1b559707f0ed4cf2a0f9405203dec3ef3f83a
amostungu <amostungu1@gmail.com>
2023-08-12 08:34:16 -0700

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,12 @@
 -- Write a script that prints the full description of the table first_table from the database hbtn_0c_0
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY
+SELECT CONCAT(
+    'first_table',
+    'CREATE TABLE `first_table` (',
+    GROUP_CONCAT(
+        CONCAT('`', COLUMN_NAME, '`', ' ', COLUMN_TYPE, ' ', IF(IS_NULLABLE = 'NO', 'NOT NULL', 'DEFAULT NULL'), ' ', IFNULL(COLUMN_DEFAULT, ''), ' ', IF(COLUMN_KEY = 'PRI', 'PRIMARY KEY', '')),
+        ',\n'
+    ),
+    ') ENGINE=', ENGINE, ' DEFAULT CHARSET=', CHARACTER_SET_NAME
+)
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
