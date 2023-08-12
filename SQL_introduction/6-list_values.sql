84848f42561d848fee40ec820cbbf0070cd5b25f
amostungu <amostungu1@gmail.com>
2023-08-12 09:18:41 -0700

    List all in table

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,2 +1,8 @@
--- a script that lists all rows of the table first_table from the database hbtn_0c_0
-SELECT * FROM hbtn_0c_0.first_table;
+-- Create the database hbtn_0c_0 if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- Use the database hbtn_0c_0
+USE hbtn_0c_0;
+
+-- List all rows from the table first_table in the database hbtn_0c_0
+SELECT * FROM first_table;
