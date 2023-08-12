6d0992030507553b3ab008f44df2872b3dea7993
amostungu <amostungu1@gmail.com>
2023-08-12 11:09:16 -0700

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,12 +1,2 @@
--- Print the full description of the table first_table without using DESCRIBE or EXPLAIN
-SELECT CONCAT(
-    'first_table',
-    'CREATE TABLE `first_table` (',
-    GROUP_CONCAT(
-        CONCAT('`', COLUMN_NAME, '`', ' ', COLUMN_TYPE, ' ', IF(IS_NULLABLE = 'NO', 'NOT NULL', 'DEFAULT NULL'), ' ', IFNULL(COLUMN_DEFAULT, ''), ' ', IF(COLUMN_KEY = 'PRI', 'PRIMARY KEY', '')),
-        ',\n'
-    ),
-    ') ENGINE=', ENGINE, ' DEFAULT CHARSET=', CHARACTER_SET_NAME
-)
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+-- a script that prints the full description of the table first_table from the database hbtn_0c_0
+SHOW COLUMNS FROM first_table
