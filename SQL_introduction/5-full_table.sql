e07803ee7c2642d1473401be35ce8da0ba583da9
amostungu <amostungu1@gmail.com>
2023-08-12 08:30:52 -0700

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 -- Write a script that prints the full description of the table first_table from the database hbtn_0c_0
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
