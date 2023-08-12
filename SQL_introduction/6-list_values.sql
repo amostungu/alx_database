c70dee41ef625e994e202e13148e8306e57433dc
amostungu <amostungu1@gmail.com>
2023-08-12 09:21:10 -0700

    List all in table

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,8 +1,2 @@
--- Create the database hbtn_0c_0 if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
--- Use the database hbtn_0c_0
-USE hbtn_0c_0;
-
 -- List all rows from the table first_table in the database hbtn_0c_0
-SELECT * FROM first_table;
+SELECT * FROM hbtn_0c_0.first_table;
