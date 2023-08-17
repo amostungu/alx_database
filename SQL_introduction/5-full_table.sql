dac046cb8f5c7771a70694488d2272cbd26957d6
amostungu <amostungu1@gmail.com>
2023-08-16 22:23:51 -0700

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- a script that prints the full description of the table first_table from the database hbtn_0c_0
-first_table CREATETABLE first_table(id int NOT NULL AUTO_INCREMENT, name varchar(128) DEFAULT NULL, c char(1) DEFAULT NULL, created_at date DEFAULT NULL, PRIMARY KEY (id) )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci
+SHOW CREATE TABLE first_table;
